ee22235ab0d69bc3d70ad63bc009178b86dd05fb
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2010-01-21 16:23:07 +0100

    initial import

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,14 @@
+grails.plugin.repos.discovery.pillarone = "https://readplugins:readplugins@svn.intuitive-collaboration.com/GrailsPlugins/"
+
+grails.plugin.repos.resolveOrder = ['pillarone', 'default', 'core']
+
+grails.compiler.dependencies = {
+    fileset(dir: "${grailsSettings.projectPluginsDir}", includes: "*/web-app/lib/*.jar")
+    fileset(dir: "${basedir}/web-app", includes: "lib/*.jar")
+}
+
+//Change paths to desired risk analytics plugin locations
+
+grails.plugin.location.'risk-analytics-core' = "../RiskAnalyticsCore"
+grails.plugin.location.'risk-analytics-application' = "../RiskAnalyticsApplication"
+grails.plugin.location.'risk-analytics-life' = "../RiskAnalyticsLife"
