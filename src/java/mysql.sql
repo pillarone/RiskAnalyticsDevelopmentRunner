fc6d14954cb958df032e60da1a2d0da0f9c68667
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2010-01-26 09:55:03 +0100

    added db initialization script

---
+++ src/java/mysql.sql
@@ -0,0 +1,60 @@
+--
+-- dbSchema.sql - database schema to separate path (output point of model)
+--                from collector (attached drainage pipes) and field
+--                attribute of each output paket. In addition, index
+--                numbers to distinguish components of a vector output
+--                are introduced
+--
+--
+drop database if exists DATABASE_NAME;
+create database DATABASE_NAME;
+
+create table DATABASE_NAME.single_value_result (
+id bigint not null auto_increment,
+version bigint not null,
+iteration int not null,
+path_id bigint not null,
+collector_id bigint not null,
+value_index int not null,
+field_id bigint not null,
+period int not null,
+simulation_run_id bigint not null,
+value double not null,
+primary key(id, simulation_run_id)
+) engine MyISAM
+partition by key(simulation_run_id)
+partitions 100;
+create index single_value_result_x1 on DATABASE_NAME.single_value_result(simulation_run_id, period, path_id, collector_id, field_id, value, iteration);
+create index single_value_result_x2 on DATABASE_NAME.single_value_result(simulation_run_id, period, path_id, collector_id, field_id, iteration, value);
+
+create table DATABASE_NAME.path_mapping (
+id bigint not null auto_increment primary key,
+version bigint not null,
+path_name varchar(255) not null
+) engine MyISAM;
+create unique index path_mapping_x1 on DATABASE_NAME.path_mapping(path_name);
+
+create table DATABASE_NAME.collector_mapping (
+id bigint not null auto_increment primary key,
+version bigint not null,
+collector_name varchar(255) not null
+) engine MyISAM;
+create unique index collector_mapping_x1 on DATABASE_NAME.collector_mapping(collector_name);
+
+create table DATABASE_NAME.field_mapping (
+id bigint not null auto_increment primary key,
+version bigint not null,
+field_name varchar(255) not null
+) engine MyISAM;
+create unique index field_x1 on DATABASE_NAME.field_mapping(field_name);
+
+--
+-- view to access all data via symbolic names
+--
+create view DATABASE_NAME.symbolic_value_result as
+select s.id, p.path_name as 'path', c.collector_name as 'collector', f.field_name as 'field', s.simulation_run_id, s.period, s.value, s.iteration
+from DATABASE_NAME.single_value_result s, DATABASE_NAME.path_mapping p, DATABASE_NAME.collector_mapping c, DATABASE_NAME.field_mapping f
+where s.path_id = p.id
+and s.collector_id = c.id
+and s.field_id = f.id
+;
---
+++ src/java/sqlserver.sql
@@ -0,0 +1,97 @@
+--
+-- dbSchema.sql - database schema to separate path (output point of model)
+--                from collector (attached drainage pipes) and field
+--                attribute of each output paket. In addition, index
+--                numbers to distinguish components of a vector output
+--                are introduced
+--
+--
+drop database DATABASE_NAME;
+create database DATABASE_NAME;
+
+CREATE TABLE [DATABASE_NAME].[dbo].[single_value_result] (
+	[id] [numeric](19, 0) IDENTITY(1,1) NOT NULL,
+	[version] [numeric](19, 0) NOT NULL,
+	[collector_id] [numeric](19, 0) NULL,
+	[field_id] [numeric](19, 0) NULL,
+	[iteration] [int] NOT NULL,
+	[path_id] [numeric](19, 0) NOT NULL,
+	[period] [int] NOT NULL,
+	[simulation_run_id] [numeric](19, 0) NOT NULL,
+	[value] [float] NOT NULL,
+	[value_index] [int] NOT NULL,
+PRIMARY KEY CLUSTERED ( [id] ASC ) WITH (PAD_INDEX  = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY];
+
+
+CREATE NONCLUSTERED INDEX [single_value_result_x1] ON [DATABASE_NAME].[dbo].[single_value_result]
+(
+	[simulation_run_id] ASC,
+	[period] ASC,
+	[path_id] ASC,
+	[collector_id] ASC,
+	[field_id] ASC,
+	[value] ASC,
+	[iteration] ASC
+) WITH (PAD_INDEX  = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF) ON [PRIMARY];
+
+CREATE NONCLUSTERED INDEX [single_value_result_x2] ON [DATABASE_NAME].[dbo].[single_value_result]
+(
+	[simulation_run_id] ASC,
+	[period] ASC,
+	[path_id] ASC,
+	[collector_id] ASC,
+	[field_id] ASC,
+	[iteration] ASC,
+	[value] ASC
+) WITH (PAD_INDEX  = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF) ON [PRIMARY];
+
+
+CREATE TABLE [DATABASE_NAME].[dbo].[path_mapping](
+	[id] [numeric](19, 0) IDENTITY(1,1) NOT NULL,
+	[version] [numeric](19, 0) NOT NULL,
+	[path_name] [varchar](255) COLLATE Latin1_General_CI_AS NOT NULL,
+PRIMARY KEY CLUSTERED ( [id] ASC ) WITH (PAD_INDEX  = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY];
+
+CREATE UNIQUE NONCLUSTERED INDEX [path_mapping_x1] ON [DATABASE_NAME].[dbo].[path_mapping]
+(
+	[path_name] ASC
+) WITH (PAD_INDEX  = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF) ON [PRIMARY]
+
+CREATE TABLE [DATABASE_NAME].[dbo].[collector_mapping](
+	[id] [numeric](19, 0) IDENTITY(1,1) NOT NULL,
+	[version] [numeric](19, 0) NOT NULL,
+	[collector_name] [varchar](255) COLLATE Latin1_General_CI_AS NOT NULL,
+PRIMARY KEY CLUSTERED ( [id] ASC ) WITH (PAD_INDEX  = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY];
+
+CREATE UNIQUE NONCLUSTERED INDEX [collector_mapping_x1] ON [DATABASE_NAME].[dbo].[collector_mapping]
+(
+	[collector_name] ASC
+) WITH (PAD_INDEX  = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF) ON [PRIMARY]
+
+CREATE TABLE [DATABASE_NAME].[dbo].[field_mapping](
+	[id] [numeric](19, 0) IDENTITY(1,1) NOT NULL,
+	[version] [numeric](19, 0) NOT NULL,
+	[field_name] [varchar](255) COLLATE Latin1_General_CI_AS NOT NULL,
+PRIMARY KEY CLUSTERED ( [id] ASC ) WITH (PAD_INDEX  = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY];
+
+CREATE UNIQUE NONCLUSTERED INDEX [field_mapping_x1] ON [DATABASE_NAME].[dbo].[field_mapping]
+(
+	[field_name] ASC
+) WITH (PAD_INDEX  = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF) ON [PRIMARY];
+
+--
+-- view to access all data via symbolic names
+--
+EXEC DATABASE_NAME..sp_executesql N'
+CREATE VIEW [dbo].[symbolic_value_result]
+AS
+SELECT DATABASE_NAME.dbo.single_value_result.id, DATABASE_NAME.dbo.single_value_result.simulation_run_id, DATABASE_NAME.dbo.single_value_result.iteration, DATABASE_NAME.dbo.single_value_result.period,
+                      DATABASE_NAME.dbo.path_mapping.path_name, DATABASE_NAME.dbo.field_mapping.field_name, DATABASE_NAME.dbo.collector_mapping.collector_name
+FROM DATABASE_NAME.dbo.collector_mapping INNER JOIN
+                      DATABASE_NAME.dbo.single_value_result ON DATABASE_NAME.dbo.collector_mapping.collector_name = DATABASE_NAME.dbo.single_value_result.collector_id INNER JOIN
+                      DATABASE_NAME.dbo.field_mapping ON DATABASE_NAME.dbo.single_value_result.field_id = DATABASE_NAME.dbo.field_mapping.field_name INNER JOIN
+                      DATABASE_NAME.dbo.path_mapping ON DATABASE_NAME.dbo.single_value_result.path_id = DATABASE_NAME.dbo.path_mapping.path_name';
