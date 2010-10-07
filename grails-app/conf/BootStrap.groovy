f2d9adfe07cd3223fa382874a2e32888b053aef3
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2010-10-07 16:06:47 +0200

    cleaning up

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,20 @@
+import org.pillarone.riskanalytics.core.parameter.comment.Tag
+import org.springframework.transaction.TransactionStatus
+
 class BootStrap {
 
-     def init = { servletContext ->
-     }
-     def destroy = {
-     }
+    def init = {servletContext ->
+        Tag.withTransaction { TransactionStatus status ->
+            if (Tag.count() < 4) {
+                new Tag(name: "todo").save()
+                new Tag(name: "fixed").save()
+                new Tag(name: "warning").save()
+                new Tag(name: "error").save()
+                new Tag(name: "review").save()
+            }
+        }
+    }
+
+    def destroy = {
+    }
 } 
