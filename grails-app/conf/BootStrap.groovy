17a7739bd83f01aab0dde0b4f8bfc2a510307292
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2010-11-05 08:24:39 +0100

    Populate table for comment tags

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,20 +1,39 @@
+import grails.plugins.springsecurity.SpringSecurityService
+import org.pillarone.riskanalytics.core.user.UserManagement
 import org.pillarone.riskanalytics.core.parameter.comment.Tag
 import org.springframework.transaction.TransactionStatus
 
 class BootStrap {
 
-    def init = {servletContext ->
+
+    SpringSecurityService authenticateService = UserManagement.getSpringSecurityService()
+
+    def init = { servletContext ->
+
         Tag.withTransaction { TransactionStatus status ->
             if (Tag.count() < 4) {
-                new Tag(name: "todo").save()
-                new Tag(name: "fixed").save()
-                new Tag(name: "warning").save()
-                new Tag(name: "error").save()
-                new Tag(name: "review").save()
+                Tag todo = new Tag(name: "todo")
+                save todo
+                Tag fixed = new Tag(name: "fixed")
+                save fixed
+                Tag warning = new Tag(name: "warning")
+                save warning
+                Tag error = new Tag(name: "error")
+                save error
+                Tag review = new Tag(name: "review")
+                save review
             }
         }
+
     }
 
-    def destroy = {
+     def destroy = {
+     }
+
+    private def save(domain) {
+        if (!domain.save()) {
+            domain.errors.each { println it }
+            throw new RuntimeException("Cannot save ${domain.class} in BootStrap")
+        }
     }
-} 
+}
